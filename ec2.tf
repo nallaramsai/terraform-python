a97d703290ee5ee2218c45600ac71adfeb9abc0e
nallaramsai <nallaramsai5@gmail.com>
2024-11-14 18:22:14 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "instance-wordpress" {
   ami = "ami-0984f4b9e98be44bf"
   instance_type = "t2.micro"
-  key_name = "ram"
+  key_name = "Ram"
   subnet_id = aws_subnet.public_sb_1.id
   vpc_security_group_ids = [aws_security_group.security1.id]
   associate_public_ip_address = true
